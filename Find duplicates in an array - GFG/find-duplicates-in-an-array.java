0e3f0656b644f7cf69f82357f5b98184f1956c5f
Prem Danav <106393065+premdanav@users.noreply.github.com>
2023-09-07 23:23:14 +0530

    Added solution - LeetHub

---
+++ Find duplicates in an array - GFG/find-duplicates-in-an-array.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            Solution g = new Solution();
+            ArrayList<Integer> ans = g.duplicates(a, n);
+            for (Integer val : ans) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> duplicates(int arr[], int n) {
+         ArrayList<Integer> list=new ArrayList<>();
+         int [] temp=new int[n];
+         for(int i=0;i<n;i++){
+             temp[arr[i]]++;
+         }
+          
+         for(int i=0;i<temp.length;i++)
+{
+    if(temp[i]>=2){
+        list.add(i);
+    }
+    
+}
+
+if(list.size()==0){
+    list.add(-1);
+    return list;
+}
+ 
+return list;
+    }
+}
