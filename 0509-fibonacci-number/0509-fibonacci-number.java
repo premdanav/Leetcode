4133b8d09d2ae2f57d633b42b95e557909134e29
Prem Danav <106393065+premdanav@users.noreply.github.com>
2023-09-17 16:04:11 +0530

    Time: 6 ms (47.96%), Space: 38.9 MB (87.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,17 +2,23 @@ class Solution {
     public int fib(int n) {
         if(n<=1)
             return n;
-        int f0=0;
-        int f1=1;
+//         int f0=0;
+//         int f1=1;
         
-        int sum=0;
+//         int sum=0;
         
-       while(n>1){
-           sum=f0+f1;
-           f0=f1;
-           f1=sum;
-           n--;
-       }
+//        while(n>1){
+//            sum=f0+f1;
+//            f0=f1;
+//            f1=sum;
+//            n--;
+//        }
+//         return sum;
+        
+        int f0=fib(n-1);
+        int f1=fib(n-2);
+        int sum=f0+f1;
         return sum;
+        
     }
 }
