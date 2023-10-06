7df7b2e65f762228497ac1ae166bc61f8ac0d704
Prem Danav <106393065+premdanav@users.noreply.github.com>
2023-10-07 02:39:32 +0530

    Time: 1 ms (99.09%), Space: 44.2 MB (77.87%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        int totalJumps=0;
+        int destination=nums.length-1;
+        int coverage=0;
+        int lastJumpIndex=0;
+        if(nums.length==1) return 0;
+        for(int i=0;i<nums.length;i++){
+            coverage=Math.max(coverage,i+nums[i]);
+
+            if(i==lastJumpIndex){
+                lastJumpIndex=coverage;
+                totalJumps++;
+
+                if(coverage>=destination)
+                    return totalJumps;
+            }
+        }
+        return totalJumps;
+    }
+}
